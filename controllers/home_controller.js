047fba6b9aa344c9c8c6b9cef92efcc5e2b4a666
dhruv-aggarwal92 <daggarwal4_be21@thapar.edu>
2023-06-03 16:08:46 +0530

    add_post_with_user_on_home

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -3,16 +3,25 @@ const post = require("../models/post");
 
 module.exports.home = async(req,res)=>{
     // console.log(req.cookies);
-    try{
-        const posts= await post.find({})
-        res.cookie('user_id',24);
-        return res.render('home',{
-            title: 'MYproj',
-            cust: pass.user,
-            user_post: posts
-        });
-    }catch(err){
+    // try{
+    //     const posts= await post.find({})
+    //     res.cookie('user_id',24);
+    //     return res.render('home',{
+    //         title: 'MYproj',
+    //         cust: pass.user,
+    //         user_post: posts
+    //     });
+    // }catch(err){
+    //     console.log("error in fetching post");
+    // }
+    post.find({}).populate('user').then((posts)=>{
+            return res.render('home',{
+                title: 'MYproj',
+                cust: pass.user,
+                user_post: posts
+            })
+        }).catch((err)=>{
         console.log("error in fetching post");
-    } 
+    });
 }
 
