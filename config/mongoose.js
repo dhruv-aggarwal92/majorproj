3b378f18b627d80e2d45bd28290f2b34e558dffe
dhruv-aggarwal92 <daggarwal4_be21@thapar.edu>
2023-07-11 16:42:59 +0530

    mongo2

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const env =  require('./environment')
-mongoose.connect(`mongodb://localhost/${env.db}`);
-
+// mongoose.connect(`mongodb://127.0.0.1:27017/${env.db}`);
+mongoose.connect(`mongodb://103.235.106.226:2737/${env.db}`);
 const db= mongoose.connection;
 
 db.on('error',console.error.bind(console,'error connecting to db'));
