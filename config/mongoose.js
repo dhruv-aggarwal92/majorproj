30e93074a6b04ccc99062c8cb9df475307f5ee95
dhruv-aggarwal92 <daggarwal4_be21@thapar.edu>
2023-07-11 15:51:22 +0530

    change_in_mongoos

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const env =  require('./environment')
-mongoose.connect(`mongodb://127.0.0.1:27017/${env.db}`);
+mongoose.connect(`mongodb://0.0.0.0:27017/${env.db}`);
 
 const db= mongoose.connection;
 
